6a22b658cfc6066811054f1c3501a0aa0010e4d8
roshni1234 <roshnivikram23@gmail.com>
2025-11-10 09:29:15 +0500

    Add files via upload

---
+++ roshnilabtask9_.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	//char name[]="Kinza";
+	//char lastName[]="khan";
+	//strlen
+	//printf("%s length is %d\n", name,strlen(name));
+		//strcat
+	//printf("%s\n", strcat(name,lastName));
+	//strncat
+	//printf("%s\n",strncat(lastName,name,3));
+	//printf("%s\n", strcpy(name, lastName));
+	//strncpy
+	//printf("%s\n", strncpy(name, lastName, strlen(lastName)-2));
+   //printf("%s\n", strncpy(name, lastName,2));
+//printf("%s\n",name, lastName+ strlen(lastName)-2);
+return 0;
+}
